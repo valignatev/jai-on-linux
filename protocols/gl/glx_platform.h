37c397163bdcf61f86e193845524b02c2bbeb4aa
Valentin Ignatev <valentignatev@gmail.com>
2025-11-10 02:35:49 +0100

    glx bindings

---
+++ protocols/gl/glx_platform.h
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+#define Bool int
+#define Status int
+#define True 1
+#define False 0
+
+typedef unsigned long XID;
+
+struct _XDisplay;
+typedef struct _XDisplay Display;
+
+
